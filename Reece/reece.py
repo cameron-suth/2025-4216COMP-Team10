37bec54daf1a04e6f4ee7756db4ec6d610d03dbd
Rmckenna1255 <reecemck1255@gmail.com>
2025-04-03 15:26:26 +0100

    Added menu to select which graph to View and linked my file to the Main.py

---
+++ Reece/reece.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+excel = pd.read_csv("M:\\CSW\\2025-4216COMP-Team10\\Spotify_dataset.csv")
+def question1():
+    column1 = excel.playlist_genre
+    column2 = excel.track_popularity
+    fig, ax = plt.subplots(figsize=(8, 6), layout="constrained", facecolor="pink")
+    ax.barh(column1, column2)
+    plt.title("based off the Genres most popular Song, which Genre is the most Popular?")
+    plt.ylabel("Genre")
+    plt.xlabel("Popularity")
+
+#question1()
+def question2():
+    column1 = excel.valence
+    column2 = excel.danceability
+    fig, ax = plt.subplots(figsize=(8, 6), layout="constrained", facecolor="pink")
+    ax.scatter(column1, column2)
+    plt.title("Does an icrease in valence equate to an increase in dancability?")
+    #ax.plot([0,1],[0,1], color="Red")
+    plt.xlabel("Valence")
+    plt.ylabel("Danceability")
+userInput = input("\nEnter the menu number for the visuallisation/records you want to view\n\n1) based off the Genres most popular Song, which Genre is the most Popular?\n2) Does an icrease in valence equate to an increase in dancability?\n3) View Both\n\nInput: ")
+def main():
+    match userInput:
+        case '1':
+            question1()
+            plt.show()
+        case '2':
+            question2()
+            plt.show()
+        case '3':
+            question1()
+            question2()
+            plt.show()
+main()
